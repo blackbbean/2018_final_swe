7f9c4927e7b7bf940780f4c5f599c93e727e7d0d
blackbbean <mrtty2000@gmail.com>
2018-01-09 19:50:07 +0900

    print

--- src/final_2018_winter/MyCalc.java
+++ src/final_2018_winter/MyCalc.java
@@ -1,13 +1,13 @@
 package final_2018_winter;
 
+import java.util.Scanner;
+
 public class MyCalc {
 
 	public static void main(String[] args) {
 		
 		
 		// TODO Auto-generated method stub
-
-		
 	}
 
 	
@@ -24,5 +24,41 @@ public class MyCalc {
 	public int subtract(int input1,int input2) {
 		int result = input1-input2;
 		return result;
+	}
+	
+	public void print() {
+		
+		System.out.println("[ID: 1774889]");
+		System.out.println("[Name:김민정]");
+		System.out.println("");
+		while (true){
+			System.out.println("1.Add two numbers");
+			System.out.println("2.Subtract twon numbers");
+			System.out.println("3.Multiply twon numbers");
+			System.out.println("4.Quit");
+			Scanner in =new Scanner(System.in);
+		
+			int choice = in.nextInt();
+			if (choice==4)
+				break;
+			else {
+				System.out.println("input two numbers:");
+				int number1 = in.nextInt();
+				int number2 = in.nextInt();
+				int result = -100;
+				if(choice==1) 
+					result =add(number1,number2);
+				else if(choice==2) 
+					result=subtract(number1,number2);
+				else if(choice==3)
+					result=multiply(number1,number2);
+				else
+					System.out.println("Exception Occurred");
+				
+				System.out.println(result);
+			}
+			
+		}
+		
 	}
 }
