6d36da67f2be790c9f5a4188ee340760811aa9c6
blackbbean <mrtty2000@gmail.com>
2018-01-09 20:06:20 +0900

    arrangement

--- src/final_2018_winter/MyCalc.java
+++ src/final_2018_winter/MyCalc.java
@@ -4,36 +4,30 @@ import java.util.Scanner;
 
 public class MyCalc {
 
-	public static void main(String[] args) {
-		
-		// TODO Auto-generated method stub
-	}
-
-	
-	public int add(int input1,int input2) {
+	public static int add(int input1,int input2) {
 		int result = input1+input2;
 		return result;
 	}
 	
-	public int multiply(int input1,int input2) {
+	public static int multiply(int input1,int input2) {
 		int result = input1*input2;
 		return result;
 	}
 	
-	public int subtract(int input1,int input2) {
+	public static int subtract(int input1,int input2) {
 		int result = input1-input2;
 		return result;
 	}
 	
-	public void print() {
+	public static void print() {
 		
 		System.out.println("[ID: 1774889]");
 		System.out.println("[Name:김민정]");
 		System.out.println("");
 		while (true){
 			System.out.println("1.Add two numbers");
-			System.out.println("2.Subtract twon numbers");
-			System.out.println("3.Multiply twon numbers");
+			System.out.println("2.Subtract two numbers");
+			System.out.println("3.Multiply two numbers");
 			System.out.println("4.Quit");
 			Scanner in =new Scanner(System.in);
 		
@@ -41,8 +35,10 @@ public class MyCalc {
 			if (choice==4)
 				break;
 			else {
-				System.out.println("input two numbers:");
+				System.out.println("input two numbers");
+				System.out.println("x:");
 				int number1 = in.nextInt();
+				System.out.println("y:");
 				int number2 = in.nextInt();
 				int result = -100;
 				if(choice==1) 
@@ -54,10 +50,18 @@ public class MyCalc {
 				else
 					System.out.println("Exception Occurred");
 				
-				System.out.println(result);
+				System.out.println("Result:"+result);
 			}
 			
 		}
 		
 	}
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		print();
+	}
+
+	
 }
